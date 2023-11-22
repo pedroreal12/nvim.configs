c8609b28d5e74cac1eaebf0b77c151e3a59ed6d1
pedroreal12 <pedrofilipereal12@gmail.com>
2023-11-22 14:37:59 +0000

    added some remaps for window handling

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -25,3 +25,11 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-s>", "<C-w><C-s>")
+vim.keymap.set("n", "<C-p>", "<C-w><C-p>")
+vim.keymap.set("n", "<C-q>", "<C-w><C-q>")
