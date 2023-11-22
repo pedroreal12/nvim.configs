4a14f334757a1976f213fd949d634260da570fff
pedroreal12 <pedrofilipereal12@gmail.com>
2023-11-22 20:44:58 +0000

    added new keymaps for the lsp plugin and added some comments for the keymaps that needed to be on the plugin configuration file due to local varibles in the keymap.lua file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,41 +1,44 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	lsp_zero.default_keymaps({buffer = bufnr})
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {"tsserver", "html", "intelephense", "clangd", "jedi_language_server", "sqlls"},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    ensure_installed = { "tsserver", "html", "intelephense", "clangd", "jedi_language_server", "sqlls" },
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+    },
+    formatting = lsp_zero.cmp_format(),
+    mapping = cmp.mapping.preset.insert({
+        ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
 })
 
 lsp_zero.setup()
--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<esc>", ":noh<cr>", {noremap = true, silent = true})
+vim.keymap.set("n", "<esc>", ":noh<cr>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -21,15 +21,63 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- format file based on the lsp server
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>l", "<C-w><C-l>")
-vim.keymap.set("n", "<leader>j", "<C-w><C-j>")
-vim.keymap.set("n", "<leader>k", "<C-w><C-k>")
-vim.keymap.set("n", "<leader>h", "<C-w><C-h>")
-vim.keymap.set("n", "<leader>s", "<C-w><C-s>")
-vim.keymap.set("n", "<leader>v", "<C-w><C-v>")
-vim.keymap.set("n", "<leader>q", "<C-w><C-q>")
+-- wiwndow handling
+vim.keymap.set("n", "<leader>l", "<C-w><C-l>") -- move to the window on the right
+vim.keymap.set("n", "<leader>j", "<C-w><C-j>") -- move to the window below
+vim.keymap.set("n", "<leader>k", "<C-w><C-k>") -- move to the window above
+vim.keymap.set("n", "<leader>h", "<C-w><C-h>") -- move to window on left
+vim.keymap.set("n", "<leader>s", "<C-w><C-s>") --new window horrizontally
+vim.keymap.set("n", "<leader>v", "<C-w><C-v>") -- new window vertically
+vim.keymap.set("n", "<leader>q", "<C-w><C-q>") -- quit window
+vim.keymap.set("n", "<leader>a", "ggVG")       --basically a CTRL + A selects the whole file in visual mode so you can yarn (copy) or delete the whole file
+
+-- plugins keymap
+-- fugitive
+-- vim.keymap.set("n", "<leader>gs", vim.cmd.Git) same as git status
+--
+-- harpoon
+--vim.keymap.set("n", "<leader>a", mark.add_file)
+--vim.keymap.set("n", "<C-p>", ui.toggle_quick_menu)
+--
+--vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+--vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+--vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+--vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+--
+--lsp_zero
+--
+--vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts) get definition
+--vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts) word search
+--vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts) view diagnostic
+--vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts) first occurence
+--vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts) last occurence
+--vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts) suggests a fix
+--vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts) view references
+--vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts) change "this" occurence
+--
+-- telescope
+--
+--vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+--vim.keymap.set('n', '<C-g>', builtin.git_files, {})
+--vim.keymap.set('n', '<leader>ps' searh with the given string in Grep >
+--
+--
+-- undotree
+--
+-- vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) shows history of the current file
+--
+--
+--['<Up>'] = cmp.mapping.select_prev_item(cmp_select), previous autocomplete
+--['<C-k>'] = cmp.mapping.select_prev_item(cmp_select), same as ^^
+--['<Down>'] = cmp.mapping.select_next_item(cmp_select), next autocomplete
+--['<C-j>'] = cmp.mapping.select_next_item(cmp_select), same as  ^^
+--['<Enter>'] = cmp.mapping.confirm({ select = true }), accept autocomplete
+--['<Tab>'] = cmp.mapping.confirm({ select = true }), same as ^^
+--['<C-y>'] = cmp.mapping.confirm({ select = true }),same as ^^
+--['<C-Space>'] = cmp.mapping.complete(),
