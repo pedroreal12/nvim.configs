f60dcfbe035683b44e4d443ab01f24bafb740244
pedroreal12 <pedrofilipereal12@gmail.com>
2023-11-28 10:44:09 +0000

    removed useless mapping <leader>d

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -17,9 +17,6 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
-vim.keymap.set("v", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- format file based on the lsp server
 
