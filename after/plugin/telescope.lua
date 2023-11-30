38dacf4c0420f90a5ff3b4e27230e889611da4fa
pedroreal12 <pedrofilipereal12@gmail.com>
2023-11-30 19:39:37 +0000

    removed plugin folder from the repository

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,18 @@
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
+telescope.setup({
+    vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case'
+    },
+})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-g>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({search = vim.fn.input("Grep > ")})
+end)
