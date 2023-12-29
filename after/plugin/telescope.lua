dfb8e7748e642a4d7e785e29dd74bbbc1a75d25f
pedroreal12 <pedrofilipereal12@gmail.com>
2023-12-29 20:06:11 +0000

    feat: grep_string now works in visual mode by selection and in normal mode with what's underneath the cursor feat: updated the telescope version in packer

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -13,6 +13,4 @@ telescope.setup({
 })
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({search = vim.fn.input("Grep > ")})
-end)
+vim.keymap.set({ "n", "v" }, '<leader>ps', builtin.grep_string, {})
