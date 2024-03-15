d6c18518e9f1c232186631277d3b197c38cec9d0
pedroreal12 <pedrofilipereal12@gmail.com>
2024-03-15 09:41:38 +0000

    fix: removed an esc remap to remove highlights when searching with / or ?

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set({"n", "v"}, "j", "jzz")
 vim.keymap.set({"n", "v"}, "k", "kzz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gvzz")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gvzz")
-vim.keymap.set("n", "<esc>", ":noh<cr>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
