6d38400f37bfc109f1c21ca4113b8ddf60fc4794
pedroreal12 <pedrofilipereal12@gmail.com>
2024-07-02 10:57:33 +0100

    added a command to be able to push

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gb", "<cmd>Git branch<CR>")
 vim.keymap.set("n", "<leader>gl", "<cmd>Git log<CR>")
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
